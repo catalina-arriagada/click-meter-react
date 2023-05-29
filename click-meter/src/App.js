fb8ff1bd767d8c52901871a92acc4a5322944cca
Catalina Arriagada <catalina.arriagada1@gmail.com>
2023-05-28 23:00:29 -0400

    app functionality

--- click-meter/src/App.js
+++ click-meter/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
+import { useState } from 'react';
+import { getImage } from './utils/utils';
 
 function App() {
+  //Hooks. useState adds states. setClicksNumber is another state
+  const [clicksNum, setClicksNumber] = useState(0); //0 is an initial value
+
+  const doClick = () => {
+    //function returns this
+    setClicksNumber(clicksNum + 1);
+  };  
+
+  const rebootCounter = () => {
+    setClicksNumber(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo-container'>
+        <img src={getImage()} alt='logo' className='logo' />
+      </div>
+      <div className='main-container'>
+        <Counter
+          clicksNum={clicksNum}
+          />
+        <Button
+          text='Click'
+          isClickBtn= {true}
+          manageClick= {doClick}
+         />
+        <Button
+          text='Reboot'
+          isClickBtn= {false}
+          manageClick= {rebootCounter}
+         />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
